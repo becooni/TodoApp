797dcf6bdada96e3dcae44cf33bfe04827b83e8d
becooni <junghum21@gmail.com>
2021-03-25 23:31:50 +0900

    test

--- app/src/test/java/com/becooni/todoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/becooni/todoapp/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.becooni.todoapp
 
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test() {
+        assertThat(true, `is`(true))
     }
 }
